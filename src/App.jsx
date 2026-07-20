3fb36b81f065a4c304615d4ace335d43515c5009
maki0315-1617 <ronron201907@gmail.com>
2026-07-20 12:47:59 +0900

    直し１－１

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
 import React from 'react';
 import './App.css';
-import floorTexture from './floor_texture.jpg';
 
 function App() {
   return (
-    <div 
-      className="game-container" 
-      style={{ backgroundImage: `url(${floorTexture})` }}
-    >
+    <div className="game-container">
       <div className="game-content">
         <h1>PKゲーム（準備中）</h1>
         <p>床の背景が表示されています。ここから機能を追加していきます。</p>
